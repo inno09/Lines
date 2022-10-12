4fd79e3d5cd22bbde13d4b637643923494ed15a4
inno <innocent.ongeri@student.moringaschool.com>
2022-10-13 00:20:38 +0300

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,18 @@
 puts "🌱 Seeding data..."
 
 15.times do 
-    Author.create(name: Faker::Book.author)
+    Author.create(
+        name: Faker::Book.author,
+        image: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "bmp", set: "set1", bgset: "bg1"),
+        description: Faker::Books::Dune.quote
+    )
 end
 
 5.times do 
-    Genre.create(name: Faker::Book.genre)
+    Genre.create(
+        name: Faker::Book.genre,
+        description: Faker::Fantasy::Tolkien.poem
+    )
 end
 
 24.times do
@@ -21,8 +28,8 @@ end
         content: Faker::Quote.matz,
         author_id: rand(1...15),
         genre_id: rand(1...5),
-        upvotes: rand(1...300),
-        downvotes: rand(1...300)
+        upvotes: rand(1...1000),
+        downvotes: rand(1...1000)
     )
 end
 
