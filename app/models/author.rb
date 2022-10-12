12ea55c1348b42e177006aadb8f9fd85f3b8233b
inno <innocent.ongeri@student.moringaschool.com>
2022-10-12 23:55:18 +0300

    created models and added validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+    validates :description, presence: true
+    validates :description, length: { minimum:10}
+
+    has_many :poems
+    has_many :genres, through: :poems
 end
--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,3 +1,9 @@
 class Poem < ApplicationRecord
-    
+    validates :content, presence: true
+    validates :title, presence: true
+    validates :content, length: { minimum:20}
+
+    belongs_to :author
+    belongs_to :genre
+
 end
