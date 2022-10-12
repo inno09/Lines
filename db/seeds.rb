973cfd7d2500c00de6614d6c97e216a9cb0b1824
inno <innocent.ongeri@student.moringaschool.com>
2022-10-12 23:47:34 +0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "🌱 Seeding data..."
+
+15.times do 
+    Author.create(name: Faker::Book.author)
+end
+
+5.times do 
+    Genre.create(name: Faker::Book.genre)
+end
+
+24.times do
+    Poem.create(
+        title: Faker::Book.title,
+        content: Faker::Quote.matz,
+        author_id: rand(1...15),
+        genre_id: rand(1...5),
+        upvotes: rand(1...300),
+        downvotes: rand(1...300)
+    )
+end
+
+puts "🌱 Done seeding!"
