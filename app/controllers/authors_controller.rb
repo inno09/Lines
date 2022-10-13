94cad0f8b5d4cfc9a2e495e6117978691d5e1f31
inno <innocent.ongeri@student.moringaschool.com>
2022-10-13 09:25:35 +0300

    updated authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,10 @@
 class AuthorsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :response_not_found
+    def index 
+        render json: Author.all, status: :ok
+    end
+    private
+    def response_not_found
+        render json: {error: "Author not found"}, status: :not_found
+    end
 end
