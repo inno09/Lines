eac7d5d64c255ca7d2968594c72965c4562dcd74
inno <innocent.ongeri@student.moringaschool.com>
2022-10-13 22:09:02 +0300

    update index. js

--- client/src/components/Explore.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Explore() {
-  return <div>Search</div>;
-}
-
-export default Explore;
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div  className="background">
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-  </div>
+  </React.StrictMode>,
+ document.getElementById('root')
 );
 
 reportWebVitals();
