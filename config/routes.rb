3e80f1d8be0aa8d4744fb65bf8419a58345c7491
inno <innocent.ongeri@student.moringaschool.com>
2022-10-13 13:58:21 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :genres
+  resources :genres, only:[:index]
   resources :authors, only:[:index]
-  resources :poems
+  resources :poems, only: [:index, :show, :create, :update, :destroy]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
