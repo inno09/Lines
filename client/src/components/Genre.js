e19019c53e8d9b49fbe51c507fb038b97a31816e
inno <innocent.ongeri@student.moringaschool.com>
2022-10-13 13:58:08 +0300

    created genres api and displayed

--- client/src/components/Genre.js
+++ client/src/components/Genre.js
@@ -3,25 +3,32 @@ import React, {useEffect, useState} from "react";
 function Genre() {
 
   const [genres, setGenre] = useState([]);
+  const {id, title, description, poem} = genres
 
   useEffect(() => {
-    fetch("http://localhost:9292/genres")
+    fetch("/genres")
     .then(response => response.json())
     .then((data) => setGenre(data));
     }, []);
-
     return (
       <div>
        <div className="author">
          {genres.map((genre) => (
-           <div className="author-dets">
+           <div className="author-dets" key={genre.id}>
              <div>
                <h2>{genre.name}</h2>
              </div>
-             {/* <div>
-               <h3>Poem:{poem.title}</h3>
-               <p>Content:{poem.content}</p>
-             </div> */}
+             <div>
+            <p>Description:{genre.description}</p>
+          </div>
+             <div>
+             {/* <ul style={{ color: "red" }}>
+              {genres.poems.map((poem) => (
+              <li key={poem}>{poem}</li>
+               ))}
+             </ul> */}
+               {/* <h3>Poem:{poem.title}</h3> */}
+             </div>
            </div>
          ))}
        </div>
